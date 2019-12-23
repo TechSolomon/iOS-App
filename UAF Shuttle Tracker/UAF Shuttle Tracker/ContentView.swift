b1c5d6759f676817e317915543c4944b2416c2cb
Spud <jeffrypig23@gmail.com>
2019-12-22 18:37:13 -0800

    Initial upload

---
+++ UAF Shuttle Tracker/UAF Shuttle Tracker/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  UAF Shuttle Tracker
+//
+//  Created by Spud on 12/22/19.
+//  Copyright © 2019 UAF-Shuttles. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
